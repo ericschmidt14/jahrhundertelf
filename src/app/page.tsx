b8703fa1896d1b4e9d1a216a60c92106055d3866
E R I C S C H M I D T <info@ericschmidt.de>
2025-03-20 07:31:34 +0100

    tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,15 +13,25 @@ export default function Home() {
   const [isGoingForward, setIsGoingForward] = useState(true);
   const [selectedPlayers, setSelectedPlayers] = useState<string[]>([]);
 
-  const goBack = () => {
-    setIsGoingForward(false);
-    setTimeout(() => setActiveStep(activeStep - 1), 100);
-  };
-
   const handleSelectPlayer = (decade: number, playerName: string) => {
     setSelectedPlayers((prev) => ({ ...prev, [decade]: playerName }));
     setIsGoingForward(true);
     setTimeout(() => setActiveStep((prev) => prev + 1), 500);
+  };
+
+  const BackButton = () => {
+    return (
+      <Button
+        variant="transparent"
+        leftSection={<IconChevronLeft size={16} />}
+        onClick={() => {
+          setIsGoingForward(false);
+          setTimeout(() => setActiveStep(activeStep - 1), 100);
+        }}
+      >
+        Zurück
+      </Button>
+    );
   };
 
   return (
@@ -50,105 +60,94 @@ export default function Home() {
               steps: { display: "none" },
             }}
           >
-            {decades.map((decade, index) => (
+            {decades.map((decade) => (
               <Stepper.Step key={decade.year} label={decade.year}>
                 <AnimatePresence mode="wait">
-                  {activeStep === index && (
-                    <motion.div
-                      key={decade.year}
-                      initial={{ opacity: 0, x: isGoingForward ? 50 : -50 }}
-                      animate={{ opacity: 1, x: 0 }}
-                      exit={{ opacity: 0, x: isGoingForward ? -50 : 50 }}
-                      transition={{ duration: 0.3, ease: "easeInOut" }}
-                      className="flex flex-col gap-8"
-                    >
-                      <i className="text-8xl text-center dimmed">
-                        {getDecade(decade.year)}
-                      </i>
-                      <div className="grid grid-cols-3 gap-4">
-                        {decade.players.map((player) => {
-                          const isSelected =
-                            selectedPlayers[decade.year] === player.name;
-                          return (
-                            <div
-                              key={player.name}
-                              className={`relative cursor-pointer transition-transform ${
-                                isSelected
-                                  ? "bg-[var(--mantine-primary-color-filled)]"
-                                  : "bg-black/20 hover:bg-[var(--mantine-primary-color-filled-hover)]"
-                              } group`}
-                              style={{
-                                clipPath:
-                                  "polygon(0% 0%, 75% 0%, 100% 20%, 100% 100%, 0% 100%)",
-                              }}
-                              onClick={() =>
-                                handleSelectPlayer(decade.year, player.name)
-                              }
-                            >
-                              <div className="relative w-full aspect-square overflow-hidden">
-                                <Image
-                                  alt={`Bild ${player.name}`}
-                                  src="/players/1.png"
-                                  layout="fill"
-                                  objectFit="cover"
-                                  className="transition-transform duration-300 group-hover:scale-105"
-                                />
-                              </div>
-                              <h3 className="text-xl text-center p-4">
-                                {player.name}
-                              </h3>
+                  <motion.div
+                    key={decade.year}
+                    initial={{ opacity: 0, x: isGoingForward ? 50 : -50 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0, x: isGoingForward ? -50 : 50 }}
+                    transition={{ duration: 0.3, ease: "easeInOut" }}
+                    className="flex flex-col gap-8"
+                  >
+                    <i className="text-8xl text-center dimmed">
+                      {getDecade(decade.year)}
+                    </i>
+                    <div className="grid grid-cols-3 gap-4">
+                      {decade.players.map((player) => {
+                        const isSelected =
+                          selectedPlayers[decade.year] === player.name;
+                        return (
+                          <div
+                            key={player.name}
+                            className={`relative cursor-pointer transition-transform ${
+                              isSelected
+                                ? "bg-[var(--mantine-primary-color-filled)]"
+                                : "bg-black/20 hover:bg-black/50"
+                            } group`}
+                            style={{
+                              clipPath:
+                                "polygon(0% 0%, 75% 0%, 100% 20%, 100% 100%, 0% 100%)",
+                            }}
+                            onClick={() =>
+                              handleSelectPlayer(decade.year, player.name)
+                            }
+                          >
+                            <div className="relative w-full aspect-square overflow-hidden">
+                              <Image
+                                alt={`Bild ${player.name}`}
+                                src="/players/1.png"
+                                layout="fill"
+                                objectFit="cover"
+                                className="transition-transform duration-300 group-hover:scale-105"
+                              />
                             </div>
-                          );
-                        })}
-                      </div>
-                      {activeStep > 0 && (
-                        <Button
-                          variant="transparent"
-                          leftSection={<IconChevronLeft size={16} />}
-                          onClick={goBack}
-                        >
-                          Zurück
-                        </Button>
-                      )}
-                    </motion.div>
-                  )}
+                            <h3 className="text-xl text-center p-4">
+                              {player.name}
+                            </h3>
+                          </div>
+                        );
+                      })}
+                    </div>
+                    {activeStep > 0 && <BackButton />}
+                  </motion.div>
                 </AnimatePresence>
               </Stepper.Step>
             ))}
-          </Stepper>
-
-          {activeStep === decades.length && (
-            <div className="max-w-[400px] m-auto flex flex-col gap-8 p-8">
-              <h2 className="text-4xl font-bold text-center">Deine Auswahl</h2>
-              <ul className="flex flex-col items-center gap-4">
-                {Object.entries(selectedPlayers).map(([decade, player]) => (
-                  <li key={decade} className="flex flex-col items-center">
-                    <i className="dimmed">{getDecade(+decade)}</i>
-                    <h3 className="text-xl">{player}</h3>
-                  </li>
-                ))}
-              </ul>
-              <div className="flex gap-2">
-                <Button
-                  variant="transparent"
-                  leftSection={<IconChevronLeft size={16} />}
-                  onClick={goBack}
-                >
-                  Zurück
-                </Button>
-                <Button
-                  leftSection={<IconSend size={16} />}
-                  onClick={() =>
-                    console.log(
-                      JSON.stringify(Object.values(selectedPlayers), null, 2)
-                    )
-                  }
-                >
-                  Auswahl abschicken
-                </Button>
+            <Stepper.Completed>
+              <div className="max-w-[480px] m-auto flex flex-col gap-8 p-8">
+                <h2 className="text-4xl font-bold text-center">
+                  Deine Auswahl
+                </h2>
+                <ul className="flex flex-col items-center gap-4">
+                  {Object.entries(selectedPlayers).map(([decade, player]) => (
+                    <li key={decade} className="flex flex-col items-center">
+                      <i className="dimmed">{getDecade(+decade)}</i>
+                      <h3 className="text-xl">{player}</h3>
+                    </li>
+                  ))}
+                </ul>
+                <div className="flex flex-col-reverse justify-center gap-2">
+                  <BackButton />
+                  <Button
+                    leftSection={<IconSend size={16} />}
+                    onClick={() =>
+                      console.log(
+                        JSON.stringify(
+                          { players: Object.values(selectedPlayers) },
+                          null,
+                          2
+                        )
+                      )
+                    }
+                  >
+                    Auswahl abschicken
+                  </Button>
+                </div>
               </div>
-            </div>
-          )}
+            </Stepper.Completed>
+          </Stepper>
         </div>
       </div>
     </div>
