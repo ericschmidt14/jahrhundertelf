c5056d36df3c6425c727f846fb3bcb2bdfd0b642
E R I C S C H M I D T <info@ericschmidt.de>
2025-03-24 11:49:24 +0100

    add tabs to result page

--- src/app/ergebnisse/page.tsx
+++ src/app/ergebnisse/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, Pagination, Table } from "@mantine/core";
+import { Button, Pagination, Table, Tabs } from "@mantine/core";
 import { IconLogout, IconTrophy } from "@tabler/icons-react";
 import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
@@ -67,90 +67,103 @@ export default function Page() {
 
   return (
     <div className="flex flex-col items-center gap-4">
-      <header className="w-full flex justify-between items-center px-8 py-2 bg-black/50">
-        <Logo />
-        <Button
-          variant="light"
-          leftSection={<IconLogout size={16} />}
-          onClick={() => signOut()}
-        >
-          Ausloggen
-        </Button>
-      </header>
-      <div className="w-full flex flex-col gap-8 p-8">
-        <Table withRowBorders={false} verticalSpacing="md">
-          <Table.Tbody>
-            {decades.map(({ year, players }) => {
-              const sortedPlayers = [...players].sort(
-                (a, b) =>
-                  (playerCounts[b.name] || 0) - (playerCounts[a.name] || 0)
-              );
+      <Tabs variant="pills" defaultValue="summary" className="w-full">
+        <header className="w-full flex justify-between items-center px-8 py-2 bg-black/50">
+          <Logo />
+          <Tabs.List>
+            <Tabs.Tab value="summary">Ergebnisse</Tabs.Tab>
+            <Tabs.Tab value="details">Abstimmungen</Tabs.Tab>
+          </Tabs.List>
+          <Button
+            variant="light"
+            leftSection={<IconLogout size={16} />}
+            onClick={() => signOut()}
+          >
+            Ausloggen
+          </Button>
+        </header>
 
-              return (
-                <Table.Tr key={year}>
-                  <Table.Td>
-                    <i className="text-xl dimmed">{year}er</i>
-                  </Table.Td>
-                  {sortedPlayers.map(({ name, given }, index) => (
-                    <Table.Td key={name}>
-                      <div className="flex items-center gap-2">
-                        {index === 0 && (
-                          <IconTrophy
-                            size={64}
-                            stroke={1}
-                            color="var(--mantine-color-orange-9)"
-                          />
-                        )}
-                        <div className="flex flex-col items-start">
-                          <h3
-                            className={`text-xl ${
-                              index === 0 &&
-                              "text-[var(--mantine-color-orange-6)]"
-                            }`}
-                          >
-                            {given} {name}
-                          </h3>
-                          <p
-                            className={`${
-                              index === 0
-                                ? "text-[var(--mantine-color-orange-9)]"
-                                : "dimmed"
-                            }`}
-                          >
-                            {playerCounts[name] || 0}x gewählt
-                          </p>
-                        </div>
-                      </div>
-                    </Table.Td>
-                  ))}
+        <Tabs.Panel value="summary">
+          <div className="p-8">
+            <Table withRowBorders={false} verticalSpacing="md">
+              <Table.Tbody>
+                {decades.map(({ year, players }) => {
+                  const sortedPlayers = [...players].sort(
+                    (a, b) =>
+                      (playerCounts[b.name] || 0) - (playerCounts[a.name] || 0)
+                  );
+
+                  return (
+                    <Table.Tr key={year}>
+                      <Table.Td>
+                        <i className="text-xl dimmed">{year}er</i>
+                      </Table.Td>
+                      {sortedPlayers.map(({ name, given }, index) => (
+                        <Table.Td key={name}>
+                          <div className="flex items-center gap-2">
+                            {index === 0 && (
+                              <IconTrophy
+                                size={64}
+                                stroke={1}
+                                color="var(--mantine-color-orange-9)"
+                              />
+                            )}
+                            <div className="flex flex-col items-start">
+                              <h3
+                                className={`text-xl ${
+                                  index === 0 &&
+                                  "text-[var(--mantine-color-orange-6)]"
+                                }`}
+                              >
+                                {given} {name}
+                              </h3>
+                              <p
+                                className={`${
+                                  index === 0
+                                    ? "text-[var(--mantine-color-orange-9)]"
+                                    : "dimmed"
+                                }`}
+                              >
+                                {playerCounts[name] || 0}x gewählt
+                              </p>
+                            </div>
+                          </div>
+                        </Table.Td>
+                      ))}
+                    </Table.Tr>
+                  );
+                })}
+              </Table.Tbody>
+            </Table>
+          </div>
+        </Tabs.Panel>
+        <Tabs.Panel value="details">
+          <div className="w-full flex flex-col gap-8 p-8">
+            <Table>
+              <Table.Thead>
+                <Table.Tr>
+                  <Table.Th>Token</Table.Th>
+                  <Table.Th>Spieler</Table.Th>
                 </Table.Tr>
-              );
-            })}
-          </Table.Tbody>
-        </Table>
-        <Table>
-          <Table.Thead>
-            <Table.Tr>
-              <Table.Th>Token</Table.Th>
-              <Table.Th>Spieler</Table.Th>
-            </Table.Tr>
-          </Table.Thead>
-          <Table.Tbody>
-            {currentPageData.map((result) => (
-              <Table.Tr key={result.token}>
-                <Table.Td className="font-mono">{result.token}</Table.Td>
-                <Table.Td>{result.players.join(", ")}</Table.Td>
-              </Table.Tr>
-            ))}
-          </Table.Tbody>
-        </Table>
-        <Pagination
-          value={page}
-          onChange={setPage}
-          total={totalPages}
-          className="flex justify-center"
-        />
-      </div>
+              </Table.Thead>
+              <Table.Tbody>
+                {currentPageData.map((result) => (
+                  <Table.Tr key={result.token}>
+                    <Table.Td className="font-mono">{result.token}</Table.Td>
+                    <Table.Td>{result.players.join(", ")}</Table.Td>
+                  </Table.Tr>
+                ))}
+              </Table.Tbody>
+            </Table>
+            <Pagination
+              value={page}
+              onChange={setPage}
+              total={totalPages}
+              className="flex justify-center"
+            />
+          </div>
+        </Tabs.Panel>
+      </Tabs>
     </div>
   );
 }
