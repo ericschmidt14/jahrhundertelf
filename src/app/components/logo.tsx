a1fc4d6140055cd5b9016fbe565864ed5a2652cf
E R I C S C H M I D T <info@ericschmidt.de>
2025-03-26 09:16:39 +0100

    add wappen

--- src/app/components/logo.tsx
+++ src/app/components/logo.tsx
@@ -3,12 +3,7 @@ import Image from "next/legacy/image";
 export default function Logo() {
   return (
     <div className="flex items-center gap-1">
-      <Image
-        src="/logo_transparent.svg"
-        width={48}
-        height={48}
-        alt="1. FCN Logo"
-      />
+      <Image src="/wappen.png" width={100} height={54} alt="1. FCN Logo" />
       <h1 className="text-2xl">
         Jahrhundert+25<i>Elf</i>
       </h1>
